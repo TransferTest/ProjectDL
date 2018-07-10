09fc97aa67ffe4a41035346e3a517b51abc309e7
jj139811 <jj139811@kaist.ac.kr>
2018-07-10 22:47:44 +0900

    전투(임시)
    임시로 전투 모양을 만들었음. 세부 사항은 이하 참고
    - 스킬 버튼 상시 비활성화
    - 스킵 버튼 삭제
    - 캐릭터 패널을 클릭하면 공격 버튼이 활성화되도록 변경
    - 공격 이후 해당 캐릭터 패널 비활성화
    - 네 캐릭터 모두 공격하면 초기화
    - 몬스터는 한 마리. 이후 시간이 있으면 두 마리로 추가 예정
    - 몬스터의 HP는 1000
    - 모든 공격의 데미지는 100
    - 몬스터의 HP가 0이 되면 결과창으로 바로 넘어가도록 변경
    - 몬스터 HP 잔량은 몬스터 아래 슬라이더에 표시
    - 현재 몬스터의 턴은 없음 이후 시간이 있으면 추가 예정

--- Sample/Assets/Scripts/SceneManagers/SceneManager_encounter.cs
+++ Sample/Assets/Scripts/SceneManagers/SceneManager_encounter.cs
@@ -2,18 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //this is scene manager for "encounter" scene
 public class SceneManager_encounter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int num_enemies;
+    public GameObject monster;
+    public int monster_HP;
+    public int monster_max;
+    public Slider monster_bar;
+    public Button panel_1;
+    public Button panel_2;
+    public Button panel_3;
+    public Button panel_4;
+    public Button attackButton;
+
+    private Button[] panels;
+    private int chain;
+
+    private bool[] selected;
+    private int cur;
+
+    // Use this for initialization
+    void Start () {
+        chain = 0;
+        panels = new Button[4];
+        panels[0] = panel_1;
+        panels[1] = panel_2;
+        panels[2] = panel_3;
+        panels[3] = panel_4;
+
+        selected = new bool[4];
+        selected[0] = false;
+        selected[1] = false;
+        selected[2] = false;
+        selected[3] = false;
+
+        GlobalControl gc = GameObject.Find("GlobalControl").GetComponent<GlobalControl>();//find the GlobalControl object
+        SceneInfo si = gc.sceneInformation;
+
+        Debug.Log("Load Scene");
+
+        if (si == null)//if there is no scene information (it might means this scene is first loaded)
+        {
+            Debug.Log("no scene information");
+            return;
+        }
+        //if there is scene information
+        Debug.Log("loaded");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        monster_bar.value = monster_HP;
+
+        if (chain >= 4)
+        {
+            selected[0] = false;
+            selected[1] = false;
+            selected[2] = false;
+            selected[3] = false;
+
+            panel_1.interactable = true;
+            panel_2.interactable = true;
+            panel_3.interactable = true;
+            panel_4.interactable = true;
+
+            chain = 0;
+        }
+
+        if (monster_HP <= 0)
+            loadResult();
 	}
 
     //loads result scene
@@ -22,5 +82,26 @@ public class SceneManager_encounter : MonoBehaviour {
         GlobalControl gc = GameObject.Find("GlobalControl").GetComponent<GlobalControl>();
         gc.save();
         SceneManager.LoadScene("result");
+    }
+
+    public void panelSelected(int n)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (selected[i])
+                continue;
+            panels[i].interactable = true;
+        }
+        panels[n - 1].interactable = false;
+        attackButton.interactable = true;
+        cur = n;
+    }
+
+    public void attack()
+    {
+        monster_HP -= 100;
+        chain++;
+        attackButton.interactable = false;
+        selected[cur - 1] = true;
     }
 }
